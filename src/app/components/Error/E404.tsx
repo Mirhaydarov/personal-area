fef5e099836f8ba5f851169be068528060b416fd
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-12-02 07:57:45 +0500

    feat: add E404 component
    E404 will display when page is not found

---
+++ src/app/components/Error/E404.tsx
@@ -0,0 +1,22 @@
+// Core
+import { FC, ReactElement } from 'react';
+
+// Components
+import { BaseLink } from '../Base/Link';
+
+// Route
+import { book } from '../../routes/book';
+
+// Styles
+import './e404.css';
+
+export const E404: FC = (): ReactElement => (
+  <article className="error">
+    <div className="wrap">
+      <h2 className="error__title">Page Not Found</h2>
+      <p className="error__desc">We can't find the page you're looking for.</p>
+      <p className="error__desc">You can either return to the previous page, or visit our home page.</p>
+      <BaseLink msg="Visit Homepage" to={book.root} className="error__link-to-home" />
+    </div>
+  </article>
+)
