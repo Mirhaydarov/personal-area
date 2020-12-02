0f008380ab20301b2d7f2da600436cad0cd63727
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-12-02 08:37:35 +0500

    feat: add Personal component
    Show users their contacts

---
+++ src/app/components/Personal/index.tsx
@@ -0,0 +1,87 @@
+// Core
+import  { FC, ReactElement, useContext, useState } from 'react';
+
+// Hooks
+import { useContactsFetch } from './hooks/useContactsFetch';
+
+// Components
+import { BaseLink } from '../Base/Link';
+import { BaseButton } from '../Base/Button';
+import { BaseSearchBar } from '../Base/SearchBar';
+import { Authorization } from '../Error/Authorization';
+
+// Actions
+import { fetchContacts } from '../../init/actions';
+
+// Instruments
+import { ContextApp } from '../../init/reducer';
+
+// Api
+import { api } from '../../api';
+
+// Path
+import { book } from '../../routes/book';
+
+// Styles
+import './contactListTile.css';
+
+export const Personal: FC = (): ReactElement => {
+  const [disabled, setDisabled] = useState(false);
+  const { contactsList } = useContactsFetch();
+  const { state, dispatch } = useContext(ContextApp);
+
+  if (!Array.isArray(contactsList)) {
+    return <Authorization />
+  }
+
+  const deleteHandler = (id: number = 0) => {
+    setDisabled(true);
+
+    const newContacts = state.contacts.filter(contact => contact.id !== id);
+    dispatch(fetchContacts(newContacts));
+
+    setTimeout(() => {
+      setDisabled(false);
+      api.deleteContactFetch(id);
+    }, 650);
+  };
+
+  const contacts = contactsList.map(({ id, name, email, job }) => (
+    <ul key={id} className="contacts__list">
+      <li>{name}</li>
+      <li className="contacts__item-email">{email}</li>
+      <li className="contacts__item-job">{job}</li>
+      <li>
+        <div className="contacts__list-box">
+          <BaseLink
+            className="contacts__list-btn contacts__list-btn--edit"
+            msg="Edit"
+            to={`${book.editContact}/${id}`}
+          />
+          <BaseButton
+            className="contacts__list-btn contacts__list-btn--delete"
+            type="button"
+            msg="Delete"
+            onClick={() => deleteHandler(id)}
+            disabled={disabled}
+          />
+        </div>
+      </li>
+    </ul>
+  ));
+
+  return (
+    <main>
+      <div className="wrap">
+        <h2 className="contacts-title">Contacts</h2>
+        <div className="contacts">
+          <nav className="contacts__nav">
+            <BaseLink msg="Add contact" to={book.addContact} className="contacts__add-new-contact" />
+            <BaseSearchBar />
+          </nav>
+          {contacts}
+        </div>
+      </div>
+    </main>
+  );
+};
