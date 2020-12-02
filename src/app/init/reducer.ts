3afe36aa39e615bbef36c4498e6c3677227db7bb
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-12-02 07:49:55 +0500

    feat: add root reducer

---
+++ src/app/init/reducer.ts
@@ -0,0 +1,51 @@
+// Core
+import { createContext } from 'react';
+
+// Types 
+import { 
+  FETCH_TOKEN,
+  FETCH_CONTACTS_SUCCESS,
+  DELETE_CONTACT,
+  FIND_CONTACT,
+  InitialStateTypes,
+  ContextAppTypes,
+  RootReducerActionsTypes
+} from './types';
+
+export const initialState: InitialStateTypes = {
+  contacts: [],
+  access_token: '',
+  searchValue: '',
+};
+
+export const ContextApp = createContext<ContextAppTypes>({ state: initialState, dispatch: () => {} });
+
+export const rootReducer = (state = initialState, action: RootReducerActionsTypes): InitialStateTypes => {
+  switch(action.type) {
+    case FETCH_CONTACTS_SUCCESS: 
+      return {
+        ...state,
+        contacts: action.payload,
+      }
+
+    case FETCH_TOKEN:
+      return {
+        ...state,
+        access_token: action.payload,
+      };
+    
+    case DELETE_CONTACT:
+      return {
+        ...state,
+      };
+    
+    case FIND_CONTACT:
+      return {
+        ...state,
+        searchValue: action.payload,
+      }
+
+    default:
+      return state;
+  }
+};
